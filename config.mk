6e137dd223d2dbc7879a9587f68d2955240542e4
Boris Sirota <borisirota@gmail.com>
2016-01-07 12:41:09 +0200

    add hex codec

--- config.mk
+++ config.mk
@@ -1,2 +1,2 @@
-SOURCES= core/sjcl.js core/aes.js core/bitArray.js core/codecString.js core/codecBase64.js core/sha256.js core/ccm.js core/hmac.js core/pbkdf2.js core/bn.js core/ecc.js core/random.js core/codecArrayBuffer.js
+SOURCES= core/sjcl.js core/aes.js core/bitArray.js core/codecString.js core/codecHex.js core/codecBase64.js core/sha256.js core/ccm.js core/hmac.js core/pbkdf2.js core/bn.js core/ecc.js core/random.js core/codecArrayBuffer.js
 COMPRESS= core_closure.js
