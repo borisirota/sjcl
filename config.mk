f37cdcb7a7bdd0f5adc42ce6ed20ce389477ce02
Boris Sirota <borisirota@gmail.com>
2017-07-10 21:15:12 +0300

    add sha512

--- config.mk
+++ config.mk
@@ -1,2 +1,2 @@
-SOURCES= core/sjcl.js core/aes.js core/bitArray.js core/codecString.js core/codecHex.js core/codecBase64.js core/codecBytes.js core/sha256.js core/gcm.js core/hmac.js core/pbkdf2.js core/bn.js core/ecc.js core/random.js
+SOURCES= core/sjcl.js core/aes.js core/bitArray.js core/codecString.js core/codecHex.js core/codecBase64.js core/sha256.js core/sha512.js core/gcm.js core/hmac.js core/pbkdf2.js core/bn.js core/ecc.js core/random.js
 COMPRESS= core_closure.js
